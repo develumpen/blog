aeddc7b1a447e4acc97c23e2dcd564a71930e12d
develumpen <hi@develumpen.com>
2025-08-07 15:59:42 -0500

    redirect admin to admin/entries

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "admin", to: redirect("admin/entries")
   namespace :admin do
     resources :entries, only: %i[ index new edit create update destroy ]
   end
