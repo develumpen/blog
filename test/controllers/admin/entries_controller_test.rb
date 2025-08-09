6fcb7ad83f0c358292cd56e5507b43c83430b8da
develumpen <hi@develumpen.com>
2025-08-09 15:55:18 -0500

    basic authentication

--- test/controllers/admin/entries_controller_test.rb
+++ test/controllers/admin/entries_controller_test.rb
@@ -6,16 +6,22 @@ class Admin::EntriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
+    sign_in_as users(:one)
+
     get admin_entries_url
     assert_response :success
   end
 
   test "should get new" do
+    sign_in_as users(:one)
+
     get new_admin_entry_url
     assert_response :success
   end
 
   test "should create entry" do
+    sign_in_as users(:one)
+
     assert_difference("Entry.count") do
       post admin_entries_url, params: { entry: { body_markdown: @entry.body_markdown, draft: @entry.draft, published_at: @entry.published_at, title: @entry.title } }
     end
@@ -24,16 +30,22 @@ class Admin::EntriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    sign_in_as users(:one)
+
     get edit_admin_entry_url(@entry)
     assert_response :success
   end
 
   test "should update entry" do
+    sign_in_as users(:one)
+
     patch admin_entry_url(@entry), params: { entry: { body_html: @entry.body_html, body_markdown: @entry.body_markdown, draft: @entry.draft, published_at: @entry.published_at, title: @entry.title } }
     assert_redirected_to admin_entry_url(@entry)
   end
 
   test "should destroy entry" do
+    sign_in_as users(:one)
+
     assert_difference("Entry.count", -1) do
       delete admin_entry_url(@entry)
     end
