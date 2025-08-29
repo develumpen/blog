ebba0e13d0e96544c316d42362c171621aa5a341
develumpen <hi@develumpen.com>
2025-08-29 11:42:00 -0500

    add tests for unauthenticated users

--- test/controllers/admin/entries_controller_test.rb
+++ test/controllers/admin/entries_controller_test.rb
@@ -5,21 +5,41 @@ class Admin::EntriesControllerTest < ActionDispatch::IntegrationTest
     @entry = entries(:one)
   end
 
-  test "should get index" do
+  test "unauthenticated user should be redirected when accessing index" do
+    get admin_entries_url
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should get index" do
     sign_in_as users(:one)
 
     get admin_entries_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "unauthenticated user should be redirected when accessing new" do
+    get new_admin_entry_url
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should get new" do
     sign_in_as users(:one)
 
     get new_admin_entry_url
     assert_response :success
   end
 
-  test "should create entry" do
+  test "unauthenticated user should be redirected when creating entry" do
+    assert_no_difference("Entry.count") do
+      post admin_entries_url, params: { entry: { body_markdown: @entry.body_markdown, draft: @entry.draft, published_at: @entry.published_at, title: @entry.title } }
+    end
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should create entry" do
     sign_in_as users(:one)
 
     assert_difference("Entry.count") do
@@ -29,21 +49,41 @@ class Admin::EntriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to edit_admin_entry_url(Entry.last)
   end
 
-  test "should get edit" do
+  test "unauthenticated user should be redirected when accessing edit" do
+    get edit_admin_entry_url(@entry)
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should get edit" do
     sign_in_as users(:one)
 
     get edit_admin_entry_url(@entry)
     assert_response :success
   end
 
-  test "should update entry" do
+  test "unauthenticated user should be redirected when updating entry" do
+    patch admin_entry_url(@entry)
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should update entry" do
     sign_in_as users(:one)
 
     patch admin_entry_url(@entry), params: { entry: { body_html: @entry.body_html, body_markdown: @entry.body_markdown, draft: @entry.draft, published_at: @entry.published_at, title: @entry.title } }
     assert_redirected_to admin_entry_url(@entry)
   end
 
-  test "should destroy entry" do
+  test "unauthenticated user should be redirected when destroying entry" do
+    assert_no_difference("Entry.count") do
+      delete admin_entry_url(@entry)
+    end
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should destroy entry" do
     sign_in_as users(:one)
 
     assert_difference("Entry.count", -1) do
--- test/controllers/admin/tags_controller_test.rb
+++ test/controllers/admin/tags_controller_test.rb
@@ -5,21 +5,41 @@ class Admin::TagsControllerTest < ActionDispatch::IntegrationTest
     @tag = tags(:one)
   end
 
-  test "should get index" do
+  test "unauthenticated user should be redirected when accessing index" do
+    get admin_tags_url
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should get index" do
     sign_in_as users(:one)
 
     get admin_tags_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "unauthenticated user should be redirected when accessing new" do
+    get new_admin_tag_url
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should get new" do
     sign_in_as users(:one)
 
     get new_admin_tag_url
     assert_response :success
   end
 
-  test "should create tag" do
+  test "unauthenticated user should be redirected when creating a tag" do
+    assert_no_difference("Tag.count") do
+      post admin_tags_url, params: { tag: { name: "newTag" } }
+    end
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should create tag" do
     sign_in_as users(:one)
 
     assert_difference("Tag.count") do
@@ -29,14 +49,29 @@ class Admin::TagsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to admin_tags_url
   end
 
-  test "should get edit" do
+  test "unauthenticated user should be redirected when accessing edit" do
+    get edit_admin_tag_url(@tag)
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should get edit" do
     sign_in_as users(:one)
 
     get edit_admin_tag_url(@tag)
     assert_response :success
   end
 
-  test "should update tag" do
+  test "unauthenticated user should be redirected when updating tag" do
+    patch admin_tag_url(@tag), params: { tag: { name: "updatedTag" } }
+
+    @tag.reload
+
+    assert_equal "Tag1", @tag.name
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated user should update tag" do
     sign_in_as users(:one)
 
     patch admin_tag_url(@tag), params: { tag: { name: "updatedTag" } }
@@ -47,7 +82,15 @@ class Admin::TagsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "updatedTag", @tag.name
   end
 
-  test "should destroy tag" do
+  test "unauthenticated user should be redirected when destroying tag" do
+    assert_no_difference("Tag.count") do
+      delete admin_tag_url(@tag)
+    end
+
+    assert_redirected_to new_session_url
+  end
+
+  test "authenticated url should destroy tag" do
     sign_in_as users(:one)
 
     assert_difference("Tag.count", -1) do
