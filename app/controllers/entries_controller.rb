e8ace6289fd9a0816b9f5f3fdfa8274a3f4611db
Develumpen <44843793+develumpen@users.noreply.github.com>
2026-01-20 16:18:23 -0600

    add entry pagination (#49)

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,15 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: %i[ show ]
 
+  PER_PAGE = 5.freeze
+
   def index
-    @entries = Entry.published.filter_by_tags(params[:tags]).order(published_at: :desc)
+    @entries = Entry
+                .published
+                .filter_by_tags(params[:tags])
+                .order(published_at: :desc)
+
+    @page_count, @entries = paginate(@entries)
   end
 
   def show
@@ -12,6 +19,25 @@ class EntriesController < ApplicationController
 
   private
     def set_entry
-      @entry = Entry.find_by_slug(params[:slug])
+      @entry = Entry.find_by_slug!(params[:slug])
+    end
+
+    def current_page
+      @current_page ||= begin
+                          page = params[:page].to_i
+                          page.positive? ? page : 1
+                        end
+    end
+
+    def paginate(scope)
+      total_count = scope.unscope(:limit, :offset).count
+      total_pages = (total_count.to_f / PER_PAGE).ceil
+
+      page = [ current_page, total_pages ].min
+      offset = PER_PAGE * (page - 1)
+
+      paginated = scope.limit(PER_PAGE).offset(offset)
+
+      [ total_pages, paginated ]
     end
 end
